978b24963d02c3ed0f66b0b8f65270ad716e18a9
SimplyYat <simplyyat@gmail.com>
2026-04-04 22:56:58 +1000

    Added Windows support through OS detection and default to layer 1

--- keyboards/handwired/dactyl_manuform/5x7_rp2040/keymaps/vial/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7_rp2040/keymaps/vial/keymap.c
@@ -1,64 +1,114 @@
 #include QMK_KEYBOARD_H
+#include "os_detection.h"
+#include "deferred_exec.h"
 
-#define _DVORAK 0
-#define _ACTIONS 1
-#define _NUMPAD 2
-#define _FUNCTIONS 3
-#define MAC_SCRNSHOT LSG(KC_5)
-#define REFRESH G(KC_R)
-#define MAC_COPY G(KC_C)
-#define MAC_CUT G(KC_X)
-#define MAC_PASTE G(KC_V)
-#define MAC_PASTE_SPECIAL RCS(KC_V)
+#define _DVORAK_MAC 0
+#define _DVORAK_WINDOWS 1
+#define _ACTIONS 2
+#define _NUMPAD 3
+#define _FUNCTIONS 4
+
+// --- COMMON Short-cuts ---
 #define ZOOM_RAISE_HAND ROPT(KC_Y)
 #define ZOOM_LEAVE_MEETING G(KC_W)
 #define ZOOM_TOGGLE_VIDEO LSG(KC_V)
 #define ZOOM_TOGGLE_MUTE LSG(KC_A)
 
+// --- MAC Short-cuts (Layer 0) ---
+#define MAC_REFRESH G(KC_R)
+#define MAC_SLACK_ADD_LINK SGUI(KC_U)
+#define MAC_COPY G(KC_C)
+#define MAC_CUT G(KC_X)
+#define MAC_PASTE G(KC_V)
+#define MAC_PASTE_SPECIAL RCS(KC_V)
+#define MAC_SELECT_ALL G(KC_A)
+#define MAC_SAVE G(KC_S)
+#define MAC_SPOTLIGHT G(KC_SPC)
+#define MAC_SCRNSHOT LSG(KC_5)
+#define MAC_NEXT_WINDOW G(KC_GRV)
+#define MAC_CLOSE_OTHERS LAG(KC_W)
+
+
+// --- ACTION Short-cuts (Layer 2) ---
+#define ACT_SCREENSHOT SGUI(KC_5)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_DVORAK] = LAYOUT(
-        KC_ESC , KC_1, KC_2, KC_3, KC_4, KC_5, REFRESH,                        _______, KC_6, KC_7, KC_8,   KC_9,   KC_0,   KC_EQL,
-        KC_TAB,  KC_QUOT, KC_COMM,KC_DOT, KC_P, KC_Y, G(KC_A),                 KC_SLSH, KC_F, KC_G, KC_C,   KC_R,   KC_L,   KC_BSLS,
-        KC_CAPS, LT(_ACTIONS, KC_A), KC_O, KC_E, KC_U, KC_I, G(KC_S),                        _______, KC_D, KC_H, KC_T,   KC_N,   KC_S,  KC_MINS,
-        KC_LSFT, LGUI_T(KC_SCLN), LOPT_T(KC_Q), KC_J, KC_K, KC_X, G(KC_SPC),                     _______,   LT(_NUMPAD, KC_B), KC_M, KC_W, RALT_T(KC_V) ,RGUI_T(KC_Z), KC_RSFT,
-        KC_LCTL,KC_LGUI,KC_LBRC, KC_RBRC,                                                                   KC_SCLN, KC_GRV, KC_RALT,  KC_RCTL,
-                                            KC_SPC,KC_BSPC,                        KC_DEL, KC_ENT,
-                                            G(KC_GRV), LAG(KC_W),                  _______,  _______,
-                                            MAC_COPY, MAC_PASTE,                      MAC_CUT, MAC_PASTE_SPECIAL
+    [_DVORAK_MAC] = LAYOUT(
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, MAC_REFRESH,                        MAC_SLACK_ADD_LINK, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL,
+        KC_TAB, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, MAC_SELECT_ALL,                        KC_SLASH, KC_F, KC_G, KC_C, KC_R, KC_L, KC_BSLS,
+        KC_CAPS, LT(_ACTIONS, KC_A), KC_O, KC_E, KC_U, KC_I, MAC_SAVE,                        _______, KC_D, KC_H, KC_T, KC_N, KC_S, LT(_ACTIONS, KC_MINUS),
+        KC_LSFT, LGUI_T(KC_SCLN), LALT_T(KC_Q), KC_J, KC_K, KC_X, MAC_SPOTLIGHT,                        _______, KC_B, KC_M, KC_W, RALT_T(KC_V), RGUI_T(KC_Z), KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LBRC, KC_RBRC,                                                                   KC_SCLN, KC_GRV, KC_RALT, KC_RCTL,
+                                            KC_SPC, KC_BSPC,                        KC_DEL, KC_ENT,
+                                            MAC_NEXT_WINDOW, MAC_CLOSE_OTHERS,                        _______, _______,
+                                            MAC_COPY, MAC_PASTE,                        MAC_CUT, MAC_PASTE_SPECIAL
+        ),
+
+    [_DVORAK_WINDOWS] = LAYOUT(
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, MAC_REFRESH,                        MAC_SLACK_ADD_LINK, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL,
+        KC_TAB, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, MAC_SELECT_ALL,                        KC_SLASH, KC_F, KC_G, KC_C, KC_R, KC_L, KC_BSLS,
+        KC_CAPS, LT(_ACTIONS, KC_A), KC_O, KC_E, KC_U, KC_I, MAC_SAVE,                        _______, KC_D, KC_H, KC_T, KC_N, KC_S, LT(_ACTIONS, KC_MINUS),
+        KC_LSFT, LGUI_T(KC_SCLN), LALT_T(KC_Q), KC_J, KC_K, KC_X, MAC_SPOTLIGHT,                        _______, KC_B, KC_M, KC_W, RALT_T(KC_V), RGUI_T(KC_Z), KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LBRC, KC_RBRC,                                                                   KC_SCLN, KC_GRV, KC_RALT, KC_RCTL,
+                                            KC_SPC, KC_BSPC,                        KC_DEL, KC_ENT,
+                                            KC_APP, MAC_CLOSE_OTHERS,                        _______, _______,
+                                            MAC_COPY, MAC_PASTE,                        MAC_CUT, MAC_PASTE_SPECIAL
         ),
 
     [_ACTIONS] = LAYOUT(
-        _______,LCAG(KC_1),LCAG(KC_2), LCA(KC_D) ,LCA(KC_LEFT),LCA(KC_E) ,LCA(KC_C),                        QK_BOOT,LCA(KC_T),LCA(KC_RIGHT),LCA(KC_G),LCAG(KC_9),LCAG(KC_0),_______,
-        _______,_______,_______,_______,_______,_______,_______,                        KC_APP, _______ , KC_HOME , KC_UP ,KC_END,_______,_______,
-        _______,_______,_______,_______,_______,_______ ,_______,                       MAC_SCRNSHOT, _______ , KC_LEFT , KC_DOWN ,KC_RIGHT,_______,_______,
-        _______,_______,_______,_______,_______,_______,_______,                        QK_CLEAR_EEPROM, _______, KC_PGUP , _______ ,KC_PGDN ,_______,_______,
-        _______,_______,_______,_______,                                                                         ZOOM_TOGGLE_MUTE,ZOOM_TOGGLE_VIDEO,ZOOM_RAISE_HAND,ZOOM_LEAVE_MEETING,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            KC_MEDIA_PLAY_PAUSE,KC_KB_MUTE,
-                                             _______,_______,            KC_KB_VOLUME_UP,KC_KB_VOLUME_DOWN
+        TO(0), LCAG(KC_1), LCAG(KC_2), LCA(KC_D), LCA(KC_LEFT), LCA(KC_E), LCA(KC_C),                        MEH(KC_5), LCA(KC_T), LCA(KC_RIGHT), LCA(KC_G), LCAG(KC_9), LCAG(KC_0), _______,
+
+        _______, _______, _______, LSA(KC_B), LSA(KC_S), LSA(KC_C), _______,                        KC_APPLICATION, _______, KC_HOME, KC_UP, KC_END, _______, _______,
+        _______, _______, _______, _______, LSA(KC_P), LSA(KC_D), _______,                        ACT_SCREENSHOT, _______, KC_LEFT, KC_DOWN, KC_RIGHT, KC_RALT, _______,
+        _______, _______, _______, _______, _______, _______, QK_BOOT,                        QK_CLEAR_EEPROM, _______, KC_PGUP, _______, KC_PGDN, _______, _______,
+        _______, _______, _______, _______,                                                                   SGUI(KC_M), SGUI(KC_O), SGUI(KC_H), SGUI(KC_K),
+                                            _______, _______,                        _______, _______,
+                                            _______, LCA(KC_EQL),                        KC_MPLY, KC_KB_MUTE,
+                                            HYPR(KC_SPC), LCA(KC_MINUS),                        KC_VOLU, KC_VOLD
         ),
 
     [_NUMPAD] = LAYOUT(
-        _______,_______,_______,_______,_______,_______,_______,                        _______,KC_NUM,KC_KP_ASTERISK,KC_KP_PLUS,KC_KP_MINUS,_______,_______,
-        _______,_______,_______,_______,_______,_______,_______,                        _______,KC_KP_SLASH,KC_P7,KC_P8 ,KC_P9,_______,_______,
-        _______,_______,_______,_______,_______,_______,_______,                        _______,_______,KC_P4,KC_P5,KC_P6,_______,_______,
-        _______,_______,_______,_______,_______,_______,_______,                        _______,_______,KC_P1,KC_P2,KC_P3,_______,_______,
-        _______,_______,_______,_______,                                                                        KC_P0,KC_PDOT,_______,_______,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______
+        _______, _______, _______, _______, _______, _______, _______,                        _______, KC_NUM_LOCK, KC_KP_ASTERISK, KC_KP_PLUS, KC_KP_MINUS, _______, _______,
+        _______, _______, KC_UP, _______, _______, _______, _______,                        _______, KC_KP_SLASH, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______,
+        _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______,                        _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,                        _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______,
+        _______, _______, _______, _______,                                                                   KC_KP_0, KC_KP_DOT, _______, _______,
+                                            _______, _______,                        _______, _______,
+                                            _______, _______,                        _______, _______,
+                                            _______, _______,                        _______, _______
         ),
+
     [_FUNCTIONS] = LAYOUT(
-        _______,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,                                    KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,_______,
-        _______,_______,_______,_______,_______,_______,_______,                        _______,KC_KP_SLASH,KC_P7,KC_P8 ,KC_P9,_______,_______,
-        _______,_______,_______,_______,_______,_______,_______,                        _______,_______,KC_P4,KC_P5,KC_P6,_______,_______,
-        _______,_______,_______,_______,_______,_______,_______,                        _______,_______,KC_P1,KC_P2,KC_P3,_______,_______,
-        _______,_______,_______,_______,                                                                        KC_P0,KC_PDOT,_______,_______,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______,
-                                             _______,_______,            _______,_______
+        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                        KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______,
+        _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______,                                                                   _______, _______, _______, _______,
+                                            _______, _______,                        _______, _______,
+                                            _______, _______,                        _______, _______,
+                                            _______, _______,                        _______, _______
         )
 };
+
+uint32_t set_os_layer(uint32_t trigger_time, void* cb_arg) {
+    switch (detected_host_os()) {
+        case OS_MACOS:
+        case OS_IOS:
+            keymap_config.swap_lctl_lgui = false;
+            keymap_config.swap_rctl_rgui = false;
+            set_single_persistent_default_layer(_DVORAK_MAC);
+            break;
+        case OS_WINDOWS:
+        case OS_LINUX:
+            keymap_config.swap_lctl_lgui = true;
+            keymap_config.swap_rctl_rgui = true;
+            set_single_persistent_default_layer(_DVORAK_WINDOWS);
+            break;
+        default:
+            return 500;
+    }
+    return 0;
+}
 
 void keyboard_post_init_user(void) {
 #ifdef CONSOLE_ENABLE
@@ -72,6 +122,8 @@ void keyboard_post_init_user(void) {
     debug_keyboard=false;
     debug_mouse=false;
 #endif
+
+    defer_exec(500, set_os_layer, NULL);
 }
 
 void set_colour(uint8_t led_index, uint16_t hue, uint8_t sat, uint8_t val){
