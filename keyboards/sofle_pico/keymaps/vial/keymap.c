5713cac1c865f6c4427da4844c612e59935c8701
SimplyYat <SimplyYat@gmail.com>
2026-05-20 16:57:34 +1000

    Added both side statuses

--- keyboards/sofle_pico/keymaps/vial/keymap.c
+++ keyboards/sofle_pico/keymaps/vial/keymap.c
@@ -112,10 +112,104 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 #ifdef OLED_ENABLE
 #include "dynamic_keymap.h"
+#include "caps_word.h"
+#include "transactions.h"
+
+#ifdef SPLIT_KEYBOARD
+typedef struct {
+    bool caps_word_on;
+    uint16_t ccw_encoder;
+    uint16_t cw_encoder;
+} user_sync_state_t;
+
+user_sync_state_t user_state;
+
+void user_sync_callback(uint8_t initiator2target_buffer_size, const void *initiator2target_buffer, uint8_t target2initiator_buffer_size, void *target2initiator_buffer) {
+    if (initiator2target_buffer_size == sizeof(user_sync_state_t)) {
+        memcpy(&user_state, initiator2target_buffer, sizeof(user_sync_state_t));
+    }
+}
+
+void keyboard_post_init_user(void) {
+    if (!is_keyboard_master()) {
+        transaction_register_rpc(USER_SYNC_STATE, user_sync_callback);
+    }
+}
+
+void housekeeping_task_user(void) {
+    if (is_keyboard_master()) {
+        user_sync_state_t new_state;
+        new_state.caps_word_on = is_caps_word_on();
+        
+        uint16_t ccw = KC_TRNS;
+        uint16_t cw = KC_TRNS;
+        uint8_t slave_enc_idx = is_keyboard_left() ? 1 : 0;
+        
+        for (int i = 3; i >= 0; i--) {
+            if ((layer_state & (1UL << i)) || i == 0) {
+                if (ccw == KC_TRNS) ccw = dynamic_keymap_get_encoder(i, slave_enc_idx, false);
+                if (cw == KC_TRNS) cw = dynamic_keymap_get_encoder(i, slave_enc_idx, true);
+            }
+        }
+        new_state.ccw_encoder = ccw;
+        new_state.cw_encoder = cw;
+
+        static user_sync_state_t last_user_state = {0};
+        if (memcmp(&new_state, &last_user_state, sizeof(user_sync_state_t)) != 0) {
+            if (transaction_rpc_send(USER_SYNC_STATE, sizeof(user_sync_state_t), &new_state)) {
+                last_user_state = new_state;
+            }
+        }
+    }
+}
+#endif
 
 static uint8_t oled_current_mode = 0; // 0 = logo, 1 = typing
 
+static void oled_write_key(uint8_t key) {
+    if (key >= KC_A && key <= KC_Z) {
+        char str[2] = {(char)('A' + (key - KC_A)), '\0'};
+        oled_write(str, false);
+    } else if (key >= KC_1 && key <= KC_0) {
+        char str[2] = {(char)((key == KC_0) ? '0' : '1' + (key - KC_1)), '\0'};
+        oled_write(str, false);
+    } else {
+        switch(key) {
+            case KC_LBRC: oled_write_P(PSTR("["), false); break;
+            case KC_RBRC: oled_write_P(PSTR("]"), false); break;
+            case KC_MINS: oled_write_P(PSTR("-"), false); break;
+            case KC_EQL:  oled_write_P(PSTR("="), false); break;
+            case KC_COMM: oled_write_P(PSTR(","), false); break;
+            case KC_DOT:  oled_write_P(PSTR("."), false); break;
+            case KC_SLSH: oled_write_P(PSTR("/"), false); break;
+            case KC_SCLN: oled_write_P(PSTR(";"), false); break;
+            case KC_QUOT: oled_write_P(PSTR("'"), false); break;
+            case KC_BSLS: oled_write_P(PSTR("\\"), false); break;
+            case KC_LEFT: oled_write_P(PSTR("<"), false); break;
+            case KC_RIGHT:oled_write_P(PSTR(">"), false); break;
+            case KC_UP:   oled_write_P(PSTR("^"), false); break;
+            case KC_DOWN: oled_write_P(PSTR("v"), false); break;
+            case KC_TAB:  oled_write_P(PSTR("Tb"), false); break;
+            case KC_SPC:  oled_write_P(PSTR("_"), false); break;
+            case KC_ENT:  oled_write_P(PSTR("En"), false); break;
+            default:      oled_write_P(PSTR("?"), false); break;
+        }
+    }
+}
+
 static void oled_write_keycode(uint16_t keycode) {
+    uint16_t mod = keycode & 0xFF00;
+    uint8_t key = keycode & 0xFF;
+    if (mod != 0) {
+        if (mod == QK_LGUI || mod == QK_RGUI) oled_write_P(PSTR("W+"), false);
+        else if (mod == QK_LSFT || mod == QK_RSFT) oled_write_P(PSTR("S+"), false);
+        else if (mod == QK_LCTL || mod == QK_RCTL) oled_write_P(PSTR("C+"), false);
+        else if (mod == QK_LALT || mod == QK_RALT) oled_write_P(PSTR("A+"), false);
+        else oled_write_P(PSTR("M+"), false);
+        oled_write_key(key);
+        return;
+    }
+    
     switch (keycode) {
         case KC_VOLD: oled_write_P(PSTR("Vol-"), false); break;
         case KC_VOLU: oled_write_P(PSTR("Vol+"), false); break;
@@ -124,20 +218,16 @@ static void oled_write_keycode(uint16_t keycode) {
         case KC_MNXT: oled_write_P(PSTR("Next"), false); break;
         case KC_MPRV: oled_write_P(PSTR("Prev"), false); break;
         case KC_MUTE: oled_write_P(PSTR("Mute"), false); break;
-        case KC_UP:   oled_write_P(PSTR("Up  "), false); break;
-        case KC_DOWN: oled_write_P(PSTR("Down"), false); break;
         case KC_MS_WH_UP: oled_write_P(PSTR("ScrU"), false); break;
         case KC_MS_WH_DOWN: oled_write_P(PSTR("ScrD"), false); break;
-        case KC_TAB:  oled_write_P(PSTR("Tab "), false); break;
-        case LSFT(KC_TAB): oled_write_P(PSTR("STab"), false); break;
         case KC_TRNS:
         case KC_NO:   oled_write_P(PSTR("None"), false); break;
-        default:      oled_write_P(PSTR("??? "), false); break;
+        default:      oled_write_key(key); break;
     }
 }
 
 bool oled_task_user(void) {
-    if (last_input_activity_elapsed() < 10000) {
+    if (last_input_activity_elapsed() < 10000 || get_current_wpm() > 0) {
         if (oled_current_mode == 0) {
             oled_clear();
             oled_current_mode = 1;
@@ -157,24 +247,57 @@ bool oled_task_user(void) {
         // Encoder Status
         oled_set_cursor(0, 6);
         oled_write_P(PSTR("Encoder:\n"), false);
-
+        
         uint16_t ccw = KC_TRNS;
         uint16_t cw = KC_TRNS;
-        uint8_t enc_idx = is_keyboard_left() ? 0 : 1;
-        for (int i = 3; i >= 0; i--) {
-            if ((layer_state & (1UL << i)) || i == 0) {
-                if (ccw == KC_TRNS) ccw = dynamic_keymap_get_encoder(i, enc_idx, false);
-                if (cw == KC_TRNS) cw = dynamic_keymap_get_encoder(i, enc_idx, true);
+        
+#ifdef SPLIT_KEYBOARD
+        if (!is_keyboard_master()) {
+            ccw = user_state.ccw_encoder;
+            cw = user_state.cw_encoder;
+        } else {
+            uint8_t enc_idx = is_keyboard_left() ? 0 : 1;
+            for (int i = 3; i >= 0; i--) {
+                if ((layer_state & (1UL << i)) || i == 0) {
+                    if (ccw == KC_TRNS) ccw = dynamic_keymap_get_encoder(i, enc_idx, false);
+                    if (cw == KC_TRNS) cw = dynamic_keymap_get_encoder(i, enc_idx, true);
+                }
             }
         }
-
+#else
+        for (int i = 3; i >= 0; i--) {
+            if ((layer_state & (1UL << i)) || i == 0) {
+                if (ccw == KC_TRNS) ccw = dynamic_keymap_get_encoder(i, 0, false);
+                if (cw == KC_TRNS) cw = dynamic_keymap_get_encoder(i, 0, true);
+            }
+        }
+#endif
+        
         if (ccw == KC_TRNS && cw == KC_TRNS) {
             oled_write_P(PSTR("None      \n"), false);
         } else {
-            oled_write_keycode(ccw);
-            oled_write_P(PSTR("/"), false);
-            oled_write_keycode(cw);
-            oled_write_P(PSTR(" \n"), false); // space padding to 10 chars
+            uint16_t ccw_mod = ccw & 0xFF00;
+            uint16_t cw_mod = cw & 0xFF00;
+            uint8_t ccw_key = ccw & 0xFF;
+            uint8_t cw_key = cw & 0xFF;
+            
+            if (ccw_mod != 0 && ccw_mod == cw_mod) {
+                if (ccw_mod == QK_LGUI || ccw_mod == QK_RGUI) oled_write_P(PSTR("Gui+"), false);
+                else if (ccw_mod == QK_LSFT || ccw_mod == QK_RSFT) oled_write_P(PSTR("Sft+"), false);
+                else if (ccw_mod == QK_LCTL || ccw_mod == QK_RCTL) oled_write_P(PSTR("Ctl+"), false);
+                else if (ccw_mod == QK_LALT || ccw_mod == QK_RALT) oled_write_P(PSTR("Alt+"), false);
+                else oled_write_P(PSTR("Mod+"), false);
+                
+                oled_write_key(ccw_key);
+                oled_write_P(PSTR("/"), false);
+                oled_write_key(cw_key);
+                oled_write_P(PSTR("  \n"), false);
+            } else {
+                oled_write_keycode(ccw);
+                oled_write_P(PSTR("/"), false);
+                oled_write_keycode(cw);
+                oled_write_P(PSTR(" \n"), false);
+            }
         }
 
         // Modifiers Status
@@ -184,6 +307,14 @@ bool oled_task_user(void) {
         char mod_str[11] = "          "; // 10 spaces
         if (mods & MOD_MASK_SHIFT) { mod_str[0]='S'; mod_str[1]='f'; mod_str[2]='t'; }
         if (mods & MOD_MASK_CTRL)  { mod_str[4]='C'; mod_str[5]='t'; mod_str[6]='l'; }
+        
+        bool cw_on = false;
+#ifdef SPLIT_KEYBOARD
+        cw_on = is_keyboard_master() ? is_caps_word_on() : user_state.caps_word_on;
+#else
+        cw_on = is_caps_word_on();
+#endif
+        if (cw_on) { mod_str[8]='C'; mod_str[9]='W'; }
 
         char mod_str2[11] = "          "; // 10 spaces
         if (mods & MOD_MASK_ALT)   { mod_str2[0]='A'; mod_str2[1]='l'; mod_str2[2]='t'; }
