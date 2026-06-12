9c5c8b18aae3f7fdaf0f787b0216ddbb22e5d648
SimplyYat <SimplyYat@gmail.com>
2026-06-12 10:58:47 +1000

    Updated to set colours for set key types for sofle pico

--- keyboards/sofle_pico/keymaps/vial/keymap.c
+++ keyboards/sofle_pico/keymaps/vial/keymap.c
@@ -7,7 +7,248 @@
 
 #ifdef SPLIT_KEYBOARD
 #include "transactions.h"
+
+typedef struct {
+    bool caps_word_on;
+    uint16_t ccw_encoder;
+    uint16_t cw_encoder;
+    uint8_t led_colors[19];
+} user_sync_state_t;
+
+user_sync_state_t user_state = {
+    .caps_word_on = false,
+    .ccw_encoder = KC_TRNS,
+    .cw_encoder = KC_TRNS,
+    .led_colors = {0}
+};
 #endif
+
+// Constant color values (change these to customize the lighting)
+#define COLOR_ALPHABET_NORMAL (rgb_t){0, 140, 255}   // Light blue
+#define COLOR_ALPHABET_CAPS   (rgb_t){255, 50, 0}    // Orange
+#define COLOR_MODIFIER        (rgb_t){127, 0, 255}   // Violet
+#define COLOR_HOLD_TAP        (rgb_t){255, 0, 127}   // Pink
+#define COLOR_TAP_DANCE       (rgb_t){255, 0, 255}   // Magenta
+#define COLOR_SYMBOL          (rgb_t){191, 0, 255}   // Purple
+#define COLOR_NUMBER          (rgb_t){0, 255, 0}     // Green
+#define COLOR_DELETE          (rgb_t){255, 0, 0}     // Red (delete/backspace)
+#define COLOR_ENTER           (rgb_t){255, 90, 0}    // Gold (enter)
+#define COLOR_LAYER           (rgb_t){75, 0, 130}    // Indigo/Deep Violet
+#define COLOR_OTHER           (rgb_t){200, 160, 255} // Lavender
+#define COLOR_ARROW           (rgb_t){255, 255, 255} // Pure White (arrow keys, space, tab)
+
+bool is_caps_active(void) {
+    bool caps_word_on = false;
+#ifdef SPLIT_KEYBOARD
+    caps_word_on = is_keyboard_master() ? is_caps_word_on() : user_state.caps_word_on;
+#else
+    caps_word_on = is_caps_word_on();
+#endif
+    return host_keyboard_led_state().caps_lock || caps_word_on;
+}
+
+uint16_t get_tap_keycode_for_hold_tap(uint16_t keycode) {
+    if (IS_QK_LAYER_TAP(keycode)) {
+        return QK_LAYER_TAP_GET_TAP_KEYCODE(keycode);
+    }
+    if (IS_QK_MOD_TAP(keycode)) {
+        return QK_MOD_TAP_GET_TAP_KEYCODE(keycode);
+    }
+    return KC_NO;
+}
+
+uint8_t get_hold_tap_color_index(uint16_t keycode, bool caps_active) {
+    uint16_t tap_kc = get_tap_keycode_for_hold_tap(keycode);
+    if (tap_kc != KC_NO) {
+        uint16_t base = tap_kc;
+        if (IS_QK_MODS(tap_kc)) {
+            base = QK_MODS_GET_BASIC_KEYCODE(tap_kc);
+        }
+        if (base >= KC_A && base <= KC_Z) {
+            return caps_active ? 13 : 12; // Hold-tap + Alphabet Caps vs Alphabet Normal
+        }
+        if ((base >= KC_1 && base <= KC_0) || (base >= KC_KP_1 && base <= KC_KP_0)) {
+            return 14; // Hold-tap + Number
+        }
+        if ((base >= KC_MINUS && base <= KC_SLASH) || base == KC_NONUS_HASH || base == KC_GRAVE) {
+            return 15; // Hold-tap + Symbol
+        }
+        if (base == KC_LEFT || base == KC_DOWN || base == KC_UP || base == KC_RGHT || base == KC_SPC || base == KC_TAB) {
+            return 21; // Hold-tap + Arrow/Space/Tab
+        }
+        if (IS_QK_MODS(tap_kc) && (QK_MODS_GET_MODS(tap_kc) & MOD_MASK_SHIFT)) {
+            uint16_t basic = QK_MODS_GET_BASIC_KEYCODE(tap_kc);
+            if ((basic >= KC_1 && basic <= KC_0) || (basic >= KC_MINUS && basic <= KC_SLASH) || basic == KC_NONUS_HASH || basic == KC_GRAVE) {
+                return 15; // Hold-tap + Symbol
+            }
+        }
+    }
+    return 4; // Default Hold-tap color (Pink)
+}
+
+uint8_t get_tap_dance_color_index(uint16_t keycode, bool caps_active) {
+#ifdef VIAL_TAP_DANCE_ENABLE
+    uint8_t index = QK_TAP_DANCE_GET_INDEX(keycode);
+    vial_tap_dance_entry_t entry;
+    if (dynamic_keymap_get_tap_dance(index, &entry)) {
+        uint16_t tap_kc = entry.on_tap;
+        if (tap_kc != KC_NO && tap_kc != KC_TRNS) {
+            uint16_t base = tap_kc;
+            if (IS_QK_MODS(tap_kc)) {
+                base = QK_MODS_GET_BASIC_KEYCODE(tap_kc);
+            }
+            if (base >= KC_A && base <= KC_Z) {
+                return caps_active ? 17 : 16; // Tap-dance + Alphabet Caps vs Alphabet Normal
+            }
+            if ((base >= KC_1 && base <= KC_0) || (base >= KC_KP_1 && base <= KC_KP_0)) {
+                return 18; // Tap-dance + Number
+            }
+            if ((base >= KC_MINUS && base <= KC_SLASH) || base == KC_NONUS_HASH || base == KC_GRAVE) {
+                return 19; // Tap-dance + Symbol
+            }
+            if (base == KC_LEFT || base == KC_DOWN || base == KC_UP || base == KC_RGHT || base == KC_SPC || base == KC_TAB) {
+                return 22; // Tap-dance + Arrow/Space/Tab
+            }
+            if (IS_QK_MODS(tap_kc) && (QK_MODS_GET_MODS(tap_kc) & MOD_MASK_SHIFT)) {
+                uint16_t basic = QK_MODS_GET_BASIC_KEYCODE(tap_kc);
+                if ((basic >= KC_1 && basic <= KC_0) || (basic >= KC_MINUS && basic <= KC_SLASH) || basic == KC_NONUS_HASH || basic == KC_GRAVE) {
+                    return 19; // Tap-dance + Symbol
+                }
+            }
+        }
+    }
+#endif
+    return 5; // Default Tap-dance color (Magenta)
+}
+
+uint8_t get_color_index(uint16_t keycode, bool caps_active, bool is_base_layer) {
+    if (!is_base_layer && (keycode == KC_TRNS || keycode == KC_NO)) {
+        return 0; // Off
+    }
+    if (keycode == KC_TRNS || keycode == KC_NO) {
+        return 11; // Other
+    }
+    if (IS_QK_TAP_DANCE(keycode)) {
+        return get_tap_dance_color_index(keycode, caps_active);
+    }
+    if (IS_QK_LAYER_TAP(keycode) || IS_QK_MOD_TAP(keycode)) {
+        return get_hold_tap_color_index(keycode, caps_active);
+    }
+    if (IS_QK_TO(keycode) || IS_QK_MOMENTARY(keycode) || IS_QK_DEF_LAYER(keycode) ||
+        IS_QK_TOGGLE_LAYER(keycode) || IS_QK_ONE_SHOT_LAYER(keycode) ||
+        IS_QK_LAYER_TAP_TOGGLE(keycode) || IS_QK_PERSISTENT_DEF_LAYER(keycode)) {
+        return 10; // Layer
+    }
+    if (IS_QK_ONE_SHOT_MOD(keycode)) {
+        return 3; // Modifier
+    }
+
+    uint16_t base = keycode;
+    if (IS_QK_MODS(keycode)) {
+        base = QK_MODS_GET_BASIC_KEYCODE(keycode);
+    }
+
+    if (base >= KC_LCTL && base <= KC_RGUI) {
+        return 3; // Modifier
+    }
+    if (base == KC_LEFT || base == KC_DOWN || base == KC_UP || base == KC_RGHT || base == KC_SPC || base == KC_TAB) {
+        return 20; // Arrow keys / Space / Tab
+    }
+    if (base == KC_BSPC || base == KC_DEL) {
+        return 8; // Delete/Backspace
+    }
+    if (base == KC_ENT || base == KC_KP_ENTER) {
+        return 9; // Enter
+    }
+    if ((base >= KC_1 && base <= KC_0) || (base >= KC_KP_1 && base <= KC_KP_0)) {
+        return 7; // Number
+    }
+    if (base >= KC_A && base <= KC_Z) {
+        return caps_active ? 2 : 1; // Caps alphabet vs normal alphabet
+    }
+    if ((base >= KC_MINUS && base <= KC_SLASH) || base == KC_NONUS_HASH || base == KC_GRAVE) {
+        return 6; // Symbol
+    }
+    if (IS_QK_MODS(keycode) && (QK_MODS_GET_MODS(keycode) & MOD_MASK_SHIFT)) {
+        uint16_t basic = QK_MODS_GET_BASIC_KEYCODE(keycode);
+        if ((basic >= KC_1 && basic <= KC_0) || (basic >= KC_MINUS && basic <= KC_SLASH) || basic == KC_NONUS_HASH || basic == KC_GRAVE) {
+            return 6; // Symbol
+        }
+    }
+    return 11; // Other
+}
+
+rgb_t get_color_from_index(uint8_t color_idx) {
+    switch (color_idx) {
+        case 1:  return COLOR_ALPHABET_NORMAL;
+        case 2:  return COLOR_ALPHABET_CAPS;
+        case 3:  return COLOR_MODIFIER;
+        case 4:  return COLOR_HOLD_TAP;
+        case 5:  return COLOR_TAP_DANCE;
+        case 6:  return COLOR_SYMBOL;
+        case 7:  return COLOR_NUMBER;
+        case 8:  return COLOR_DELETE;
+        case 9:  return COLOR_ENTER;
+        case 10: return COLOR_LAYER;
+        case 11: return COLOR_OTHER;
+        case 12: {
+            rgb_t base = COLOR_ALPHABET_NORMAL;
+            rgb_t effect = COLOR_HOLD_TAP;
+            return (rgb_t){(3 * base.r + effect.r) / 4, (3 * base.g + effect.g) / 4, (3 * base.b + effect.b) / 4};
+        }
+        case 13: {
+            rgb_t base = COLOR_ALPHABET_CAPS;
+            rgb_t effect = COLOR_HOLD_TAP;
+            return (rgb_t){(3 * base.r + effect.r) / 4, (3 * base.g + effect.g) / 4, (3 * base.b + effect.b) / 4};
+        }
+        case 14: {
+            rgb_t base = COLOR_NUMBER;
+            rgb_t effect = COLOR_HOLD_TAP;
+            return (rgb_t){(3 * base.r + effect.r) / 4, (3 * base.g + effect.g) / 4, (3 * base.b + effect.b) / 4};
+        }
+        case 15: {
+            rgb_t base = COLOR_SYMBOL;
+            rgb_t effect = COLOR_HOLD_TAP;
+            return (rgb_t){(3 * base.r + effect.r) / 4, (3 * base.g + effect.g) / 4, (3 * base.b + effect.b) / 4};
+        }
+        case 16: {
+            rgb_t base = COLOR_ALPHABET_NORMAL;
+            rgb_t effect = COLOR_TAP_DANCE;
+            return (rgb_t){(3 * base.r + effect.r) / 4, (3 * base.g + effect.g) / 4, (3 * base.b + effect.b) / 4};
+        }
+        case 17: {
+            rgb_t base = COLOR_ALPHABET_CAPS;
+            rgb_t effect = COLOR_TAP_DANCE;
+            return (rgb_t){(3 * base.r + effect.r) / 4, (3 * base.g + effect.g) / 4, (3 * base.b + effect.b) / 4};
+        }
+        case 18: {
+            rgb_t base = COLOR_NUMBER;
+            rgb_t effect = COLOR_TAP_DANCE;
+            return (rgb_t){(3 * base.r + effect.r) / 4, (3 * base.g + effect.g) / 4, (3 * base.b + effect.b) / 4};
+        }
+        case 19: {
+            rgb_t base = COLOR_SYMBOL;
+            rgb_t effect = COLOR_TAP_DANCE;
+            return (rgb_t){(3 * base.r + effect.r) / 4, (3 * base.g + effect.g) / 4, (3 * base.b + effect.b) / 4};
+        }
+        case 20: return COLOR_ARROW;
+        case 21: {
+            rgb_t base = COLOR_ARROW;
+            rgb_t effect = COLOR_HOLD_TAP;
+            return (rgb_t){(3 * base.r + effect.r) / 4, (3 * base.g + effect.g) / 4, (3 * base.b + effect.b) / 4};
+        }
+        case 22: {
+            rgb_t base = COLOR_ARROW;
+            rgb_t effect = COLOR_TAP_DANCE;
+            return (rgb_t){(3 * base.r + effect.r) / 4, (3 * base.g + effect.g) / 4, (3 * base.b + effect.b) / 4};
+        }
+        default: return (rgb_t){0, 0, 0};
+    }
+}
+
+rgb_t get_keycode_color(uint16_t keycode, bool caps_active, bool is_base_layer) {
+    return get_color_from_index(get_color_index(keycode, caps_active, is_base_layer));
+}
 
 enum sofle_layers {
     _QWERTY,
@@ -117,20 +358,6 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #endif
 
 #ifdef SPLIT_KEYBOARD
-typedef struct {
-    bool caps_word_on;
-    uint16_t ccw_encoder;
-    uint16_t cw_encoder;
-    uint64_t led_mask;
-} user_sync_state_t;
-
-user_sync_state_t user_state = {
-    .caps_word_on = false,
-    .ccw_encoder = KC_TRNS,
-    .cw_encoder = KC_TRNS,
-    .led_mask = 0xFFFFFFFFFFFFFFFFULL
-};
-
 void user_sync_callback(uint8_t initiator2target_buffer_size, const void *initiator2target_buffer, uint8_t target2initiator_buffer_size, void *target2initiator_buffer) {
     if (initiator2target_buffer_size == sizeof(user_sync_state_t)) {
         memcpy(&user_state, initiator2target_buffer, sizeof(user_sync_state_t));
@@ -143,35 +370,74 @@ void keyboard_post_init_user(void) {
     }
 }
 
-uint64_t calculate_led_mask(void) {
-    uint8_t layer = get_highest_layer(layer_state);
-    if (layer == 0) {
-        return 0xFFFFFFFFFFFFFFFFULL;
+uint8_t get_slave_led_index(uint8_t i) {
+    if (is_keyboard_left()) {
+        return i + 29;
+    } else {
+        return i;
     }
-    uint64_t mask = 0;
+}
+
+void pack_color_5bit(uint8_t *arr, uint8_t led_idx, uint8_t val) {
+    uint16_t bit_offset = (uint16_t)led_idx * 5;
+    uint8_t byte_idx = bit_offset / 8;
+    uint8_t bit_idx = bit_offset % 8;
+
+    uint16_t mask = ~(0x1F << bit_idx);
+    arr[byte_idx] &= (mask & 0xFF);
+    if (bit_idx > 3) {
+        arr[byte_idx + 1] &= ((mask >> 8) & 0xFF);
+    }
+
+    arr[byte_idx] |= ((val & 0x1F) << bit_idx);
+    if (bit_idx > 3) {
+        arr[byte_idx + 1] |= ((val & 0x1F) >> (8 - bit_idx));
+    }
+}
+
+uint8_t unpack_color_5bit(const uint8_t *arr, uint8_t led_idx) {
+    uint16_t bit_offset = (uint16_t)led_idx * 5;
+    uint8_t byte_idx = bit_offset / 8;
+    uint8_t bit_idx = bit_offset % 8;
+
+    uint16_t val = arr[byte_idx];
+    if (bit_idx > 3) {
+        val |= ((uint16_t)arr[byte_idx + 1] << 8);
+    }
+    return (val >> bit_idx) & 0x1F;
+}
+
+void calculate_slave_led_colors(uint8_t *arr, bool caps_active) {
+    uint8_t layer = get_highest_layer(layer_state);
+    bool is_base = (layer == 0);
+    memset(arr, 0, 19);
+
+    uint8_t slave_min = is_keyboard_left() ? 29 : 0;
+    uint8_t slave_max = is_keyboard_left() ? 58 : 29;
+
     for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
         for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
             uint8_t index = g_led_config.matrix_co[row][col];
-            if (index != NO_LED && index < 64) {
+            if (index >= slave_min && index < slave_max) {
+                uint8_t i = index - slave_min;
                 uint16_t keycode = dynamic_keymap_get_keycode(layer, row, col);
-                if (keycode != KC_TRNS && keycode != KC_NO) {
-                    mask |= (1ULL << index);
-                }
+                uint8_t color_idx = get_color_index(keycode, caps_active, is_base);
+
+                pack_color_5bit(arr, i, color_idx);
             }
         }
     }
-    return mask;
 }
 
 void housekeeping_task_user(void) {
     if (is_keyboard_master()) {
         user_sync_state_t new_state;
         new_state.caps_word_on = is_caps_word_on();
-        
+
         uint16_t ccw = KC_TRNS;
         uint16_t cw = KC_TRNS;
         uint8_t slave_enc_idx = is_keyboard_left() ? 1 : 0;
-        
+
         for (int i = 3; i >= 0; i--) {
             if ((layer_state & (1UL << i)) || i == 0) {
                 if (ccw == KC_TRNS) ccw = dynamic_keymap_get_encoder(i, slave_enc_idx, false);
@@ -180,7 +446,7 @@ void housekeeping_task_user(void) {
         }
         new_state.ccw_encoder = ccw;
         new_state.cw_encoder = cw;
-        new_state.led_mask = calculate_led_mask();
+        calculate_slave_led_colors(new_state.led_colors, is_caps_active());
 
         static user_sync_state_t last_user_state = {0};
         if (memcmp(&new_state, &last_user_state, sizeof(user_sync_state_t)) != 0) {
@@ -189,26 +455,6 @@ void housekeeping_task_user(void) {
             }
         }
     }
-}
-#else
-uint64_t calculate_led_mask(void) {
-    uint8_t layer = get_highest_layer(layer_state);
-    if (layer == 0) {
-        return 0xFFFFFFFFFFFFFFFFULL;
-    }
-    uint64_t mask = 0;
-    for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-        for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-            uint8_t index = g_led_config.matrix_co[row][col];
-            if (index != NO_LED && index < 64) {
-                uint16_t keycode = dynamic_keymap_get_keycode(layer, row, col);
-                if (keycode != KC_TRNS && keycode != KC_NO) {
-                    mask |= (1ULL << index);
-                }
-            }
-        }
-    }
-    return mask;
 }
 #endif
 
@@ -261,7 +507,7 @@ static void oled_write_keycode(uint16_t keycode) {
         oled_write_key(key);
         return;
     }
-    
+
     switch (keycode) {
         case KC_VOLD: oled_write_P(PSTR("Vol-"), false); break;
         case KC_VOLU: oled_write_P(PSTR("Vol+"), false); break;
@@ -313,10 +559,10 @@ bool oled_task_user(void) {
         // Encoder Status
         oled_set_cursor(0, 6);
         oled_write_P(PSTR("Encoder:\n"), false);
-        
+
         uint16_t ccw = KC_TRNS;
         uint16_t cw = KC_TRNS;
-        
+
 #ifdef SPLIT_KEYBOARD
         if (!is_keyboard_master()) {
             ccw = user_state.ccw_encoder;
@@ -338,7 +584,7 @@ bool oled_task_user(void) {
             }
         }
 #endif
-        
+
         if (ccw == KC_TRNS && cw == KC_TRNS) {
             oled_write_P(PSTR("None      \n"), false);
         } else {
@@ -346,14 +592,14 @@ bool oled_task_user(void) {
             uint16_t cw_mod = cw & 0xFF00;
             uint8_t ccw_key = ccw & 0xFF;
             uint8_t cw_key = cw & 0xFF;
-            
+
             if (ccw_mod != 0 && ccw_mod == cw_mod) {
                 if (ccw_mod == QK_LGUI || ccw_mod == QK_RGUI) oled_write_P(PSTR("Gui+"), false);
                 else if (ccw_mod == QK_LSFT || ccw_mod == QK_RSFT) oled_write_P(PSTR("Sft+"), false);
                 else if (ccw_mod == QK_LCTL || ccw_mod == QK_RCTL) oled_write_P(PSTR("Ctl+"), false);
                 else if (ccw_mod == QK_LALT || ccw_mod == QK_RALT) oled_write_P(PSTR("Alt+"), false);
                 else oled_write_P(PSTR("Mod+"), false);
-                
+
                 oled_write_key(ccw_key);
                 oled_write_P(PSTR("/"), false);
                 oled_write_key(cw_key);
@@ -373,7 +619,7 @@ bool oled_task_user(void) {
         char mod_str[11] = "          "; // 10 spaces
         if (mods & MOD_MASK_SHIFT) { mod_str[0]='S'; mod_str[1]='f'; mod_str[2]='t'; }
         if (mods & MOD_MASK_CTRL)  { mod_str[4]='C'; mod_str[5]='t'; mod_str[6]='l'; }
-        
+
         bool cw_on = false;
 #ifdef SPLIT_KEYBOARD
         cw_on = is_keyboard_master() ? is_caps_word_on() : user_state.caps_word_on;
@@ -402,21 +648,44 @@ bool oled_task_user(void) {
 #endif
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    uint64_t mask;
+    bool caps_active = is_caps_active();
+    uint8_t layer = get_highest_layer(layer_state);
+    bool is_base = (layer == 0);
+
 #ifdef SPLIT_KEYBOARD
     if (is_keyboard_master()) {
-        mask = calculate_led_mask();
-    } else {
-        mask = user_state.led_mask;
-    }
-#else
-    mask = calculate_led_mask();
+#endif
+        uint8_t master_min = 0;
+        uint8_t master_max = 58;
+#ifdef SPLIT_KEYBOARD
+        master_min = is_keyboard_left() ? 0 : 29;
+        master_max = is_keyboard_left() ? 29 : 58;
 #endif
 
-    for (uint8_t i = led_min; i < led_max; i++) {
-        if (!(mask & (1ULL << i))) {
-            rgb_matrix_set_color(i, 0, 0, 0);
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+                if (index >= led_min && index < led_max && index >= master_min && index < master_max) {
+                    uint16_t keycode = dynamic_keymap_get_keycode(layer, row, col);
+                    rgb_t rgb = get_keycode_color(keycode, caps_active, is_base);
+                    rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
+                }
+            }
+        }
+#ifdef SPLIT_KEYBOARD
+    } else {
+        uint8_t slave_min = is_keyboard_left() ? 0 : 29;
+        uint8_t slave_max = is_keyboard_left() ? 29 : 58;
+
+        for (uint8_t i = led_min; i < led_max; ++i) {
+            if (i >= slave_min && i < slave_max) {
+                uint8_t idx = i - slave_min;
+                uint8_t color_idx = unpack_color_5bit(user_state.led_colors, idx);
+                rgb_t rgb = get_color_from_index(color_idx);
+                rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+            }
         }
     }
+#endif
     return false;
 }
--- keyboards/sofle_pico/keymaps/vial/readme.md
+++ keyboards/sofle_pico/keymaps/vial/readme.md
@@ -2,9 +2,49 @@
 ![SofleKeyboard adjust layer](https://i.imgur.com/f5sKy0I.png)
 
 
-# Default keymap for Sofle Pico Keyboard
+# Custom Vial Keymap for Sofle Pico Keyboard
 
-Adapted from the classic Sofle Keyboard by Josef Adamcik.
-Simplified to serve as a starting point for further customizations.
-Layout in [Keyboard Layout Editor](http://www.keyboard-layout-editor.com/#/gists/76efb423a46cbbea75465cb468eef7ff)
-Left encoder controls volume up/down/mute. Right encoder PGUP/PGDOWN.
+This is a highly customized keymap for the Sofle Pico, adapted from the classic Sofle Keyboard by Josef Adamcik, featuring advanced dynamic per-key RGB backlighting and full Vial support.
+
+## Key Features
+
+### 1. Dynamic Layer-Based Lighting
+To help with visibility and layer navigation, the lighting matches your active key configurations:
+- **Base Layer (Layer 0)**: All keys light up using their designated key category colors.
+- **Active Layers (Layers 1+)**: Only keys that are **active** (i.e. set to non-transparent/non-empty keycodes on the current layer) will light up. Empty and transparent keys (`KC_TRNS`, `KC_NO`) remain dark/off.
+
+### 2. Live EEPROM Parsing
+The firmware reads configuration changes in real-time from the keyboard's EEPROM. Any changes you make to your keymap or layers in the **Vial GUI** are instantly detected, and the per-key LED colors update automatically without needing a firmware re-compile.
+
+### 3. Split Synchronization
+Per-key LED colors are computed on the master half and synchronized to the slave half via custom packed 5-bit transactions (RPC). This guarantees consistent color representation across both halves regardless of which side is connected to USB.
+
+---
+
+## Key Color Reference
+
+The keymap classifies keycodes into distinct groups, each mapped to a harmonious color scheme:
+
+| Key Category | Color | RGB Value | Notes / Description |
+| :--- | :--- | :--- | :--- |
+| **Normal Alphabet** | Sky Blue | `(0, 140, 255)` | Standard letters (a-z) |
+| **Caps Lock / Caps Word** | Vibrant Orange | `(255, 50, 0)` | Letters when Caps Lock or Caps Word is active |
+| **Number** | Green | `(0, 255, 0)` | Number row and keypad numbers |
+| **Symbol** | Purple | `(191, 0, 255)` | Punctuation, brackets, math operators, etc. |
+| **Modifier** | Violet | `(127, 0, 255)` | `Shift`, `Ctrl`, `Alt`, `Gui`, etc. |
+| **Layer Key** | Indigo | `(75, 0, 130)` | Momentary layer switches or toggles |
+| **Delete / Backspace** | Red | `(255, 0, 0)` | `KC_DEL` and `KC_BSPC` |
+| **Enter** | Gold / Amber | `(255, 90, 0)` | Enter keys (adjusted for green hardware LED bias) |
+| **Arrow, Space, Tab** | Pure White | `(255, 255, 255)` | Directional arrows, space bar, and tab key |
+| **Other** | Lavender | `(200, 160, 255)` | Any other keys / fallback |
+
+### Special Keys (Hold-Tap, Tap-Dance, and Blends)
+
+To distinguish dual-function keys, the keymap uses special colors and color blending:
+
+* **Hold-Tap / Mod-Tap (`LT`, `MT`)**:
+  - **Standard**: Light Pink `(255, 0, 127)`
+  - **Blended**: If the primary tap action is a letter, number, or symbol, the color blends **75%** of the tap character's color with **25%** of the Hold-Tap pink.
+* **Tap-Dance**:
+  - **Standard**: Magenta `(255, 0, 255)`
+  - **Blended**: If the primary tap action is a letter, number, or symbol, the color blends **75%** of the tap character's color with **25%** of the Tap-Dance magenta.
